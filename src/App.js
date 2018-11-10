19fa3e448930a20e8d119b6bdb4514ea472aa045
Alexander Thompson <legoboyjr@gmail.com>
2018-11-10 18:31:24 -0500

    worked on nav

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header>
+          <h2 className="d-flex justify-content-center font-weight-bold bg-danger mb-0 pt-2 pb-2">Meme</h2>
+          <ul class="nav nav-pills nav-fill border-bottom bg-dark">
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">Main Memes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">Spicy Comments</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">My Memes</a>
+            </li>
+          </ul>
+        </header>
       </div>
     );
   }
